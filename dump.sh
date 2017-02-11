126151acb991054f3dfd9ba2aa97959cda42bd2a
Ivan Punko <punkoivav@yandex.ru>
2017-02-11 07:57:55 -0500

    fix typo

--- dump.sh
+++ dump.sh
@@ -7,7 +7,7 @@ echo "Job started at $(date)"
 DATE=$(date +%Y%m%d_%H%M%S)
 FILE="/dump/$PREFIX-$DATE.sql"
 
-pg_dump -h ${PGHOST} -U ${PGUSER} -f ${FILE}
+pg_dump -h "${PGHOST}" -U "${PGUSER}" -f "${FILE}" -d "${PGDATABASE}"
 gzip "$FILE"
 
 echo "Job finished at $(date)"
